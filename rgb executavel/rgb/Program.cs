68d5bc94666759c3c2f2a9b28fbde28b1e08b472
claudineiw <claudineiwickert@gmail.com>
2021-04-07 21:39:41 -0300

    executavel com permissoes

---
+++ rgb executavel/rgb/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace rgb
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);  
+            Process proc = new Process();
+            proc.StartInfo.WorkingDirectory = "System";
+            String value = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            if (value != "")
+            {
+                string[] item = value.Split(new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < item.Length; i++)
+                {
+                    string teste = item[i];
+                    if (teste.Contains("Java"))
+                    {
+                        if (teste.Contains("bin"))
+                        {
+
+                            String filename = Directory.GetCurrentDirectory() + "\\rgb.jar";
+                            proc.StartInfo.FileName = teste + "\\java.exe";
+                            proc.StartInfo.Arguments = "-jar " + filename + " %1";
+                            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            proc.StartInfo.ErrorDialog = true;
+                            proc.Start();
+                        }
+
+                    }
+
+                }
+            }
+            proc.WaitForExit();
+        }
+    }
+}
